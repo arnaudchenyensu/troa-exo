b1bf1d9028b98d27920e69b4c6d37e1be5b0b50d
Arnaud Chenyensu <contact@arnaudcys.com>
2023-02-05 17:06:25 +0100

    First pass for modal

--- src/App.tsx
+++ src/App.tsx
@@ -38,46 +38,123 @@ const beers = [
   },
 ]
 
-const renderBeers = () => {
-  return beers.map((beer, i) => {
-    const n = i % 2 === 0 ? 'even' : 'odd'
+function App() {
+  const [beerIndex, setBeerIndex] = useState(-1)
 
-    if (n === 'even') {
-      return (
-        <div key={ beer.name } className={`beer ${n}`}>
-          <div className='beer-info'>
-            <h1 className='beer-name'>{ beer.name }</h1>
-            <h4 className='beer-subtitle'>{ beer.subtitle }</h4>
-            <button className='see-more'>See more <span>+</span></button>
+  const renderBeers = () => {
+    return beers.map((beer, i) => {
+      const n = i % 2 === 0 ? 'even' : 'odd'
+
+      if (n === 'even') {
+        return (
+          <div key={ beer.name } className={`beer ${n}`}>
+            <div className='beer-info'>
+              <h1 className='beer-name'>{ beer.name }</h1>
+              <h4 className='beer-subtitle'>{ beer.subtitle }</h4>
+              <button className='see-more' onClick={() => setBeerIndex(i)}>
+                See more <span>+</span>
+              </button>
+            </div>
+            <img src={beerImage} className='beer-img' alt={`${beer.name} image`} />
           </div>
-          <img src={beerImage} className='beer-img' alt={`${beer.name} image`} />
-        </div>
-      )
-    } else {
-      return (
-        <div key={ beer.name } className={`beer ${n}`}>
-          <img src={beerImage} className='beer-img' alt={`${beer.name} image`} />
-          <div className='beer-info'>
-            <h1 className='beer-name'>{ beer.name }</h1>
-            <h4 className='beer-subtitle'>{ beer.subtitle }</h4>
-            <button className='see-more'>See more <span>+</span></button>
+        )
+      } else {
+        return (
+          <div key={ beer.name } className={`beer ${n}`}>
+            <img src={beerImage} className='beer-img' alt={`${beer.name} image`} />
+            <div className='beer-info'>
+              <h1 className='beer-name'>{ beer.name }</h1>
+              <h4 className='beer-subtitle'>{ beer.subtitle }</h4>
+              <button className='see-more' onClick={() => setBeerIndex(i)}>
+                See more <span>+</span>
+              </button>
+            </div>
           </div>
-        </div>
+        )
+      }
+    })
+  }
+
+  const modal = () => {
+    const beer = beers[beerIndex]
+
+    if (beer) {
+      return (
+        <>
+          <div className='beer-display'>
+            <h1 className='display-title'>{ beer.name }</h1>
+            <img src={beerImage} className='beer-img' alt={`${beer.name} image`} />
+          </div>
+          <div className='info'>
+            <div className='beer-title'>{ beer.name }</div>
+            <div className='beer-subtitle'>{ beer.subtitle }</div>
+            <p>Running the knife-edge between an India Pale Ale and a Stout, this particular style is one we truly love. Black IPAs are a great showcase for the skill of our brew team, balancing so many complex and twisting flavours in the same moment. The citrus, mango and pine from the hops – three of our all-time favourites – play off against the roasty dryness from the malt bill at each and every turn.</p>
+            <div>
+              <div className='specifications'>
+                <h3 className='specifications-title'>Specifications</h3>
+                <div className='attr'>
+                  <span className='attr-key'>First brewed</span>
+                  <span className='attr-val'>12/2015</span>
+                </div>
+                <div className='attr'>
+                  <span className='attr-key'>ABV</span>
+                  <span className='attr-val'>5.3</span>
+                </div>
+                <div className='attr'>
+                  <span className='attr-key'>IBU</span>
+                  <span className='attr-val'>60</span>
+                </div>
+                <div className='attr'>
+                  <span className='attr-key'>EBC / SRM</span>
+                  <span className='attr-val'>200 / 100</span>
+                </div>
+              </div>
+              <div className='ingredients'>
+                <h3 className='ingredients-title'>Ingredients</h3>
+                <div className='attr'>
+                  <div className='attr-key'>Malt</div>
+                  <div className='attr-val'>
+                    <div>Pale Ale</div>
+                    <div>Caramalt</div>
+                    <div>Crystal 150</div>
+                    <div>Carafa Special Malt Type 1</div>
+                  </div>
+                </div>
+                <div className='attr'>
+                  <div className='attr-key'>Yeast</div>
+                  <div className='attr-val'>Wyeast 1056 - American Ale</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
       )
     }
-  })
-}
+  }
 
-function App() {
+  const closeModalBtn = () => {
+    if (beerIndex !== -1) {
+      return (
+        <button className='close-modal' onClick={() => setBeerIndex(-1) }>
+          X
+        </button>
+      )
+    }
+  }
+
   return (
     <div className="App">
       <header>
         <img src={logo} className="logo" alt="logo" />
+        { closeModalBtn() }
         <span className="beer-emoji">🍺</span>
       </header>
 
       <div className="content">
-        { renderBeers() }
+        { beerIndex !== -1
+          ? <div className="modal">{ modal() }</div>
+          : <div className="beers">{ renderBeers() }</div>
+        }
       </div>
 
       <footer>
